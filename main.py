7b793ce7b2ca1b3913f58a3da55d445bb144003d
Grayson Butcher <graypbj@gmail.com>
2026-04-01 10:19:49 -0600

    Add ability to move player

--- main.py
+++ main.py
@@ -6,9 +6,60 @@ pygame.init()
 # Create screen
 screen = pygame.display.set_mode((800, 600))
 
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('assets/image.png')
+pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load('assets/player.png')
+playerX = 800 // 2 - (playerImg.get_width() // 2)
+playerY = 480
+playerX_change = 0
+playerY_change = 0
+
+# Enemy
+enemyImg = pygame.image.load('assets/enemy.png')
+enemyX = 800 // 2 - (playerImg.get_width() // 2)
+enemyY = 480
+enemyX_change = 0
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
+
+# Game Loop
 running = True
 while running:
+    # RGB - Red, Green, Blue
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        # if keystroke is pressed, check whether it is right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+    
+    if playerX < 0:
+        playerX = 0
+    elif playerX >=736:
+        playerX = 736
+
+    # Draw the player after screen has been drawn
+    player(playerX, playerY)
+
+    # Updates any changes made to the screen
+    pygame.display.update()
 
