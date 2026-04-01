df5bfc4cd0de64998a6f193849e88451961d9beb
Grayson Butcher <graypbj@gmail.com>
2026-03-31 20:36:51 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+# Initialize pygame
+pygame.init()
+
+# Create screen
+screen = pygame.display.set_mode((800, 600))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
