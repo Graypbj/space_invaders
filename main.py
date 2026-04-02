35aa1fd00d96e088ec20af06469c788acb29dcd8
Grayson Butcher <graypbj@gmail.com>
2026-04-02 11:28:52 -0600

    Add multiple enemies and bullets

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
+import math
 import random
 
 # Initialize pygame
 pygame.init()
+clock = pygame.time.Clock()
 
 # Create screen
 screen = pygame.display.set_mode((800, 600))
@@ -22,17 +24,55 @@ playerY = 480
 playerX_change = 0
 
 # Enemy
-enemyImg = pygame.image.load('assets/enemy.png')
-enemyX = random.randint(0, 800)
-enemyY = random.randint(50, 150)
-enemyX_change = 0.3
-enemyY_change = 40
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
+
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load('assets/enemy.png'))
+    enemyX.append(random.randint(0, 735))
+    enemyY.append(random.randint(50, 150))
+    enemyX_change.append(3)
+    enemyY_change.append(40)
+
+# Bullet
+bulletImg = pygame.image.load('assets/bullet.png')
+bulletX = 0
+bulletY = 480
+bulletY_change = 10
+# Ready - You can't see the bullet on the screen
+# Fire - The bullet is currently moving
+bullet_state = "ready"
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
-def enemy(x, y):
-    screen.blit(enemyImg, (x, y))
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
+
+def isCollision(enemyX, enemyY, bulletX, bulletY):
+    distance = math.sqrt((math.pow(bulletX - enemyX, 2)) + (math.pow(bulletY - enemyY, 2)))
+    if distance < 27:
+        return True
+    return False
+
+# Score
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf', 32)
+textX = 10
+textY = 10
+
+def show_score(x, y):
+    score = font.render("Score: " + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
 
 # Game Loop
 running = True
@@ -50,9 +90,15 @@ while running:
         # if keystroke is pressed, check whether it is right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.5
+                playerX_change = -5
             if event.key == pygame.K_RIGHT:
-                playerX_change = 0.5
+                playerX_change = 5
+            if event.key == pygame.K_SPACE:
+                if bullet_state == "ready":
+                    # Get the current x coordinate of the spaceship
+                    bulletX = playerX
+                    fire_bullet(bulletX, bulletY)
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -66,19 +112,44 @@ while running:
         playerX = 736
 
     # Enemy movement
-    enemyX += enemyX_change
+    for i in range(num_of_enemies):
+        enemyX[i] += enemyX_change[i]
 
-    if enemyX < 0:
-        enemyX_change = 0.3
-        enemyY += enemyY_change
-    elif enemyX >=736:
-        enemyX_change = -0.3
-        enemyY += enemyY_change
+        if enemyX[i] < 0:
+            enemyX_change[i] = 3
+            enemyY[i] += enemyY_change[i]
+        elif enemyX[i] >= 736:
+            enemyX_change[i] = -3
+            enemyY[i] += enemyY_change[i]
+
+        # Collision detection
+        collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+
+        if collision:
+            bulletY = 480
+            bullet_state = "ready"
+            score_value += 1
+            enemyX[i] = random.randint(0, 735)
+            enemyY[i] = random.randint(50, 150)
+
+        # Draw enemy
+        enemy(enemyX[i], enemyY[i], i)
+
+    # Bullet movement
+    if bulletY <= 0:
+        bulletY = 480
+        bullet_state = "ready"
+
+    if bullet_state == "fire":
+        fire_bullet(bulletX, bulletY)
+        bulletY -= bulletY_change
+
 
     # Draw the assets after screen has been drawn
     player(playerX, playerY)
-    enemy(enemyX, enemyY)
+    show_score(textX, textY)
 
     # Updates any changes made to the screen
     pygame.display.update()
+    clock.tick(60)
 
